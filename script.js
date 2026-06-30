96b08ba15b6b6e5b053ddf874992924ba0a289f2
MateoCipres <matoecipres7@gmail.com>
2026-06-30 14:56:37 -0300

    feat: crear archivo script.js y vincularlo a las paginas

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ==========================================================
+   Proyecto Integrador - Hollow Knight Silksong Platino
+   Parte 3: Interactividad y Programación con JavaScript
+   Autor: Mateo Cipres
+   ========================================================== */
+
+console.log("script.js cargado correctamente");
